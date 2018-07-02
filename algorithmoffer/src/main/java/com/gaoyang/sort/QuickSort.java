ad5e3e099fe030a2a91839becd31e92c80e3599d
Gaoyang <yang.gao@nestia.com>
2018-07-02 21:05:07 +0800

    code

--- algorithmoffer/src/main/java/com/gaoyang/sort/QuickSort.java
+++ algorithmoffer/src/main/java/com/gaoyang/sort/QuickSort.java
@@ -1,19 +1,60 @@
 package com.gaoyang.sort;
 
+import com.gaoyang.algorithmoffer.Utils;
+
 /**
  * Created by gaoyang on 2018/06/29.
  */
 public class QuickSort {
 
-    private static void quickSort(int[] arr) {
+    private static void quickSort(int[] arr, int left, int right) {
 
+        if (arr == null) {
+            return;
+        }
+
+        int key = arr[left];
+        int partition = partitionIt(arr, left+1, right, key);
+        quickSort(arr, left + 1, partition - 1);
+        quickSort(arr, partition + 1, right);
     }
 
-    private static void sort
+    private static int partitionIt(int[] arr, int left, int right, int key) {
+        System.out.println("key = "+key);
+        int initLeft = left;
+        int i = left;
+        int j = right;
+
+        while (true) {
+            while (i < j && arr[j] >= key) {
+                j--;
+            }
+
+            while (i < j && arr[i] <= key) {
+                i++;
+            }
+
+            if (arr[i] > arr[j]) {
+                Utils.swap(arr, i, j);
+            }
+
+            if (i >= j) {
+                break;
+            }
+        }
+
+        Utils.swap(arr, i, initLeft);
+
+        return i;
+    }
 
     public static void main(String[] args) {
-        int[] arr = {3,5,8,9,6,1,2,7,4};
+        int[] arr = {3, 5, 8, 9, 6, 1, 2, 7, 4};
 
-        quickSort(arr);
+        quickSort(arr, 0, arr.length - 1);
+
+        for (int i = 0; i < arr.length-1; i++) {
+            System.out.print(arr[i]+" ");
+        }
     }
 }
